c578fbabd19929d92d106ec5c48139e1795eee11
maenabuamr <maenabuamr@gmail.com>
2026-06-28 13:11:24 +0000

    Create Credit Management page

---
+++ pages/02_credit_management.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Credit Management",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("💰 Credit Management")
+
+st.info("Credit Management Module - Under Development")
