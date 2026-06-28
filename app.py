69a2bc9c5afa380d201c09897738f32b4e85fe16
maenabuamr <maenabuamr@gmail.com>
2026-06-28 17:58:41 +0000

    App test

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="TEST")
+st.set_page_config(page_title="ERP AI Analytics")
 
-st.title("🔥 BUILD 12345")
+st.title("🚀 ERP AI Analytics")
+
+st.success("إذا رأيت هذه الرسالة فهذا يعني أن التطبيق يعمل من آخر نسخة.")
